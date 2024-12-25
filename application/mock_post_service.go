3ca8ab70ae06148be1aa0fb51a1e3cb6df46b173
bandvov <bandvov@gmail.com>
2024-12-25 10:38:34 +0200

    add mocks for post service

---
+++ application/mock_post_service.go
@@ -0,0 +1,31 @@
+package application
+
+import "github.com/bandvov/social-media-go/domain"
+
+type MockPostService struct {
+	CreatePostFunc   func(post *domain.CreatePostRequest) error
+	DeletePostFunc   func(id int) error
+	UpdatePostFunc   func(id int, post *domain.Post) error
+	GetPostByIDFunc  func(id int) (*domain.Post, error)
+	FindByUserIDFunc func(userID int) ([]domain.Post, error)
+}
+
+func (s *MockPostService) CreatePost(post *domain.CreatePostRequest) error {
+	return s.CreatePostFunc(post)
+}
+
+func (s *MockPostService) DeletePost(id int) error {
+	return s.DeletePostFunc(id)
+}
+
+func (s *MockPostService) UpdatePost(id int, post *domain.Post) error {
+	return s.UpdatePostFunc(id, post)
+}
+
+func (s *MockPostService) GetPostByID(id int) (*domain.Post, error) {
+	return s.GetPostByIDFunc(id)
+}
+
+func (s *MockPostService) GetPostsByUser(userID int) ([]domain.Post, error) {
+	return s.FindByUserIDFunc(userID)
+}
