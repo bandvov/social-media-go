4f90bc521ae49866f993e56b17c6b9db6cac062a
bandvov <bandvov@gmail.com>
2025-03-01 19:03:55 +0200

    add endpoint

--- posts/main.go
+++ posts/main.go
@@ -61,11 +61,10 @@ func main() {
 	router.HandleFunc("GET /{id}", interfaces.LoggerMiddleware(postHandler.GetPost))
 	// Update post
 	router.HandleFunc("PUT /{id}", interfaces.LoggerMiddleware(postHandler.UpdatePost))
-	// Delete post
-	// this is mocked. Implement soft delete. make visibility = none
-	router.HandleFunc("DELETE /{id}", interfaces.LoggerMiddleware(postHandler.DeletePost))
 	// Get posts by user
 	router.HandleFunc("GET /user/{id}", interfaces.LoggerMiddleware(postHandler.GetPostsByUser))
+	// Delete post
+	router.HandleFunc("DELETE /{id}", interfaces.LoggerMiddleware(postHandler.DeletePost))
 
 	router.HandleFunc("GET /healthz", interfaces.LoggerMiddleware(postHandler.HealthCheckHandler))
 
