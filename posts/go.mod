e60fcf2deeb69090f750ffac5c36fc7b1115dc9a
bandvov <bandvov@gmail.com>
2025-03-30 14:44:00 +0300

    fix posts http client

--- posts/go.mod
+++ posts/go.mod
@@ -10,15 +10,23 @@ require (
 )
 
 require (
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/vmihailenco/go-tinylfu v0.2.2 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.4 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/otel v1.35.0 // indirect
+	go.opentelemetry.io/otel/metric v1.35.0 // indirect
+	go.opentelemetry.io/otel/trace v1.35.0 // indirect
 	golang.org/x/net v0.33.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
 )
 
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.60.0
 )
--- posts/internal/httpClient.go
+++ posts/internal/httpClient.go
@@ -4,8 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 	"time"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
 type ClientInterface interface {
@@ -15,8 +19,9 @@ type ClientInterface interface {
 
 // HTTPClient wraps an HTTP client with timeout settings.
 type HTTPClient struct {
-	Client   *http.Client
-	BasePath string
+	Client    *http.Client
+	BasePath  string
+	Transport http.RoundTripper
 }
 
 // NewHTTPClient initializes a new HTTP client.
@@ -26,23 +31,34 @@ func NewHTTPClient(basePath string) ClientInterface {
 		Client: &http.Client{
 			Timeout: 5 * time.Second,
 		},
-		BasePath: basePath,
+		BasePath:  basePath,
+		Transport: otelhttp.NewTransport(http.DefaultTransport),
 	}
 }
 
 // Do sends an HTTP request and returns an HTTP response.
 func (h *HTTPClient) Do(req *http.Request) (*http.Response, error) {
 	if h.BasePath != "" {
-		req.URL.Path = h.BasePath + req.URL.Path
+		// Ensure no double slashes
+		fullURL := strings.TrimRight(h.BasePath, "/") + "/" + strings.TrimLeft(req.URL.Path, "/")
+
+		parsedURL, err := url.Parse(fullURL)
+		if err != nil {
+			return nil, fmt.Errorf("invalid URL: %v", err)
+		}
+		req.URL = parsedURL
+
+		// Debug print
+		fmt.Fprintln(os.Stdout, "Final URL:", req.URL.String())
 	}
-	fmt.Fprintf(os.Stdout, "%+v", req)
+
 	return h.Client.Do(req)
 }
 
 // GetJSON performs a GET request and decodes the response into the provided struct.
 func (h *HTTPClient) GetJSON(req *http.Request, target interface{}) error {
 	fmt.Fprintf(os.Stdout, "%+v\n", req)
-	resp, err := h.Client.Do(req)
+	resp, err := h.Do(req)
 	if err != nil {
 		return err
 	}
